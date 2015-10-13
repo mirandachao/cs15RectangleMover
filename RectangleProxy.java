21fd3516d8a9f96c9c2ef39bf23b0dbfcaf47acc
mirandachao <miranda_chao@brown.edu>
2015-10-13 19:29:20 -0400

    Create RectangleProxy.java

---
+++ RectangleProxy.java
@@ -0,0 +1,29 @@
+
+public class RectangleProxy {
+	
+	private MovableRect _movingRectangle;
+	
+	public RectangleProxy() {
+		/* Constructor doesn't need to do anything because PaneOrganizer will call setHexagon(...)*/
+	}
+	
+	public void setRectangle(MovableRect rect) {
+		_movingRectangle = rect;
+	}
+	
+	public void moveUp() {
+		_movingRectangle.moveUp();
+	}
+	
+	public void moveDown() {
+		_movingRectangle.moveDown();
+	}
+	
+	public void moveRight() {
+		_movingRectangle.moveRight();
+	}
+	
+	public void moveLeft() {
+		_movingRectangle.moveLeft();
+	}
+}
